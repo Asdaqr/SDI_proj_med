29b486f9f5000944443dbe8a6367b871c9ce8129
Asdaq Rafeeque <109673335+Asdaqr@users.noreply.github.com>
2023-07-06 20:55:39 -0400

    Started front end

--- frontend.py
+++ frontend.py
@@ -3,5 +3,18 @@ import backend
 import datetime as dt
 import helper
 
-
 root = tk.Tk()
+root.geometry("1920x1080")
+
+
+# seperate root into 3 frames - top, middle, and bottom
+
+class day_of_week:
+    def __init__(self, day, frame):
+        self.day = day
+        self.checklist = tk.Listbox(frame)  # add stylization alter
+
+dov = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+for i,day in enumerate(dov):
+    pass
